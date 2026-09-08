9b66abc74a7f22bb4893c76a20e9add6d15e41b9
Pierrick Dardelet <pierrick.dardelet@etu.minesparis.psl.eu>
2026-09-08 10:18:37 +0200

    Questions 2, 3, 4, 5

--- TP-python-numerique/TP-images-1.py
+++ TP-python-numerique/TP-images-1.py
@@ -6,10 +6,18 @@ import matplotlib.pyplot as plt
 # Création de tableau puis affichage en image.
 
 T=np.empty((91,91,3))
-T=np.array(T,dtype=np.int8)
+T=np.array(T,dtype=np.int16)
 
 print(T.dtype)
 print(T)
+
+T[:,:]=[0,255,0]
+
+T[::10]=[0,0,255]
+T[:,::10]=[0,0,255]
+
+
 plt.imshow(T)
 plt.show()
 
+print(T[0,0],T[-1,-1])
